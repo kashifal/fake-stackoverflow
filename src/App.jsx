115c3e63b94f4c3636351252fcd22fae12d36553
Kashif Sulehria <captainkashi4@gmail.com>
2023-10-20 12:22:57 +0500

    all changes fixed

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@
 import FakeStackOverflow from "./components/fakestackoverflow";
 import SingleQuestion from "./components/SingleQuestion";
 import { Route, Routes } from "react-router-dom";
+import Tags from "./components/Tags";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<FakeStackOverflow />} />
       <Route path="/:id" element={<SingleQuestion />} />
+      <Route path="/tags" element={<Tags />} />
     </Routes>
   );
 }
