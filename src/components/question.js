65466b874ee06957ce21be229355daebaa4eaec6
Kashif Sulehria <captainkashi4@gmail.com>
2023-10-20 12:49:52 +0500

    nice

--- src/components/question.js
+++ src/components/question.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import Model from "../model.js";
-import { m } from "./fakestackoverflow";
+import { m } from "./fakestackoverflow.js";
 import React, { useState } from "react";
 // import QuestionForm from './QuestionForm.js';
 
